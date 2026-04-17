6f477577eb14c0bed9c6000c7791c7db37c67dbb
Jordan Estes <jordan.estes@gmail.com>
2026-04-17 15:06:01 -0500

    adds schemas for card, deck, and deck_entry

---
+++ lib/top_deck_tutor/decks/deck.ex
@@ -0,0 +1,26 @@
+defmodule TopDeckTutor.Decks.Deck do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "decks" do
+    field :name, :string
+    field :format, :string
+    field :description, :string
+    field :visibility, :string, default: "private"
+
+    belongs_to :user, TopDeckTutor.Accounts.User
+    has_many :deck_entries, TopDeckTutor.Decks.DeckEntry
+
+    timestamps()
+  end
+
+  @fields [:name, :format, :description, :visibility, :user_id]
+
+  def changeset(deck, attrs) do
+    deck
+    |> cast(attrs, @fields)
+    |> validate_required([:name, :format, :visibility, :user_id])
+    |> validate_inclusion(:visibility, ["private", "unlisted", "public"])
+    |> assoc_constraint(:user)
+  end
+end
